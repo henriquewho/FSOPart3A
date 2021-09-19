dbfcaf8e3c0957dae9783d28b37ab6dee1a17ce3
henriquewho <henriquegfonseca89@gmail.com>
2021-09-19 17:59:52 -0300

    first commit to heroku

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // require express and put it on 'app'
+const { application, request, response } = require('express');
 const express = require('express')
 let morgan = require('morgan'); 
+const cors = require('cors'); 
 const app = express(); 
 
+app.use(cors()); 
 app.use(express.json())
 //app.use(morgan('tiny'))
 
@@ -64,6 +67,7 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response)=>{
     console.log(`adding a new person`);
     const data = request.body; 
+    const number = +data.phone; 
 
     if (persons.find(each=>each.name === data.name)){
         const err = {error: 'Name must be unique'}; 
@@ -71,8 +75,8 @@ app.post('/api/persons', (request, response)=>{
     } else if (data.name=="" || !data.name){
         const err = {error: 'Person needs a name'}
         return response.status(400).send(err); 
-    } else if (!data.number || data.number<1){
-        const err = {error: 'Number must be valid'}; 
+    } else if (!number || number<1){
+        const err = {error: 'Number must be valid'};
         return response.status(400).send(err); 
     }
 
@@ -85,8 +89,13 @@ app.post('/api/persons', (request, response)=>{
     response.json(newPerson); 
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
 // define port and tell app to listen at it
-const PORT = 3001; 
+const PORT = process.env.PORT || 3001; 
 app.listen(PORT); 
 
 
